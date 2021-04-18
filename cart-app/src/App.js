f6cc66415b2b8a48e8d2bbb226aef8aed3d9b5c3
sarah-knowles <sjk339@gmail.com>
2021-04-18 21:13:01 +1200

    images loading

--- cart-app/src/App.js
+++ cart-app/src/App.js
@@ -1,22 +1,14 @@
-
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
 import './index.css';
 
 function App() {
   return (
-
-    <header className='heading'>
-      <div className='overlay'>
-        <nav className='nav'>
-          <h2>Shop</h2>
-          <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'><ion-icon name="basket-outline"></ion-icon>Cart: <span>0</span></a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-
+    <div className='App'>
+      <Header />
+      <Home />
+    </div>
   );
 }
 
