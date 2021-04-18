3222b4d39cde0b53eb2ae2e4add9b8b8386d38fc
sarah-knowles <sjk339@gmail.com>
2021-04-18 19:57:48 +1200

    initial commit

---
+++ cart-app/src/App.js
@@ -0,0 +1,23 @@
+
+import './index.css';
+
+function App() {
+  return (
+
+    <header className='heading'>
+      <div className='overlay'>
+        <nav className='nav'>
+          <h2>Shop</h2>
+          <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Cart</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+  );
+}
+
+export default App;
