177ae3817837e8464e87bc5adc0e035451ea928b
sarah-knowles <sjk339@gmail.com>
2021-04-29 12:27:19 +1200

    final commit

--- cart-app/src/actions/productQuantity.js
+++ cart-app/src/actions/productQuantity.js
@@ -1,11 +1,18 @@
-import { INCREASE_QUANTITY, DECREASE_QUANTITY } from './types';
+import { INCREASE_QUANTITY, DECREASE_QUANTITY, CLEAR_PRODUCT } from './types';
 
 export const productQuantity = (action, name) => {
   return (dispatch) => {
-    console.log('action', action)
-    console.log('name', name)
     dispatch({
       type: action === 'increase' ? INCREASE_QUANTITY : DECREASE_QUANTITY,
+      payload: name
+    })
+  }
+}
+
+export const clearProduct = (name) => {
+  return (dispatch) => {
+    dispatch({
+      type: CLEAR_PRODUCT,
       payload: name
     })
   }
